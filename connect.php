07cc800648f9aa674afae7ef3f674984d10aa32e
Repits Mark <repitsmark@Repitss-MacBook-Pro.local>
2021-01-14 23:51:20 +0100

    connect

--- connect.php
+++ connect.php
@@ -1,9 +1,9 @@
 <?php 
 // header('Content-Type: text/html; charset=utf-8');
-$host = 'mysql';
-$user = 'root';
-$pass = 'rootpassword';
-$db = 'alina_db';
+$host = 'localhost';
+$user = 'u227195896_ozd';
+$pass = 'Repka123.';
+$db = 'u227195896_wellis_ozd';
 $conn = new mysqli($host, $user, $pass, $db);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
