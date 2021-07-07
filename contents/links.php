7de01238131d18b96334ea89ffb8c5f06ded6f41
repimark <repitsm@gmail.com>
2021-07-07 10:47:06 +0200

    changed link.php

--- contents/links.php
+++ contents/links.php
@@ -4,3 +4,4 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 <link rel="stylesheet" type="text/css" href="../style.css">
+<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
